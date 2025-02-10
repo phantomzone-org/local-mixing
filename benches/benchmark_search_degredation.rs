3da24640c802b1eab8dcb8f5e485f8730d99aadd
alexlindenbaum <alexander@phantom.zone>
2025-02-09 22:48:11 -0500

    degredation test

---
+++ benches/benchmark_search_degredation.rs
@@ -0,0 +1,24 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use local_mixing::{
+    circuit::Circuit,
+    local_mixing::{consts::N_OUT_KND, LocalMixingJob},
+    replacement::strategy::ReplacementStrategy,
+};
+use rand::SeedableRng;
+use rand_chacha::ChaCha8Rng;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = ChaCha8Rng::from_os_rng();
+    let circuit = Circuit::load_from_binary("degredation.bin").unwrap();
+    let job = LocalMixingJob::new(100, 0, 100, 1, 10, ReplacementStrategy::Dummy, circuit);
+
+    c.bench_function("search degredation", |b| {
+        b.iter(|| {
+            let mut job = job.clone();
+            black_box(job.execute_step::<_, N_OUT_KND>(&mut rng));
+        });
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
